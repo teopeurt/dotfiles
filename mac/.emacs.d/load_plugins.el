4854e5e34eab98431159b8dc6c14a6340ab0fd32
dengzhp <kofreestyler@gmail.com>
2012-01-29 19:02:25 +0800

    .emacs for mac

---
+++ mac/.emacs.d/load_plugins.el
@@ -0,0 +1,44 @@
+;; color-theme
+(require 'color-theme)
+(color-theme-initialize)
+(color-theme-gray30)
+
+;;global
+(autoload 'gtags-mode "gtags" "" t)
+
+(setq gtags-mode-hook
+   '(lambda ()
+         (define-key gtags-mode-map "\eh" 'gtags-display-browser)
+         (define-key gtags-mode-map "\C-]" 'gtags-find-tag-from-here)
+         (define-key gtags-mode-map "\C-t" 'gtags-pop-stack)
+ ;;      (define-key gtags-mode-map "\el" 'gtags-find-file)
+ ;;      (define-key gtags-mode-map "\eg" 'gtags-find-with-grep)
+ ;;      (define-key gtags-mode-map "\eI" 'gtags-find-with-idutils)
+ ;;      (define-key gtags-mode-map "\es" 'gtags-find-symbol)
+ ;;      (define-key gtags-mode-map "\er" 'gtags-find-rtag)
+ ;;      (define-key gtags-mode-map "\et" 'gtags-find-tag)
+ ;;      (define-key gtags-mode-map "\ev" 'gtags-visit-rootdir)
+))
+
+
+(require 'htmlize)
+
+(add-to-list 'load-path
+              "~/.emacs.d/plugins/yasnippet")
+(require 'yasnippet) ;; not yasnippet-bundle
+(yas/global-mode 1)
+
+(require 'browse-kill-ring)
+(browse-kill-ring-default-keybindings)
+
+;;google region
+(defun google-region (beg end) "Google the selected region." (interactive "r") (
+browse-url (concat "http://www.google.com/search?ie=utf-8&oe=utf-8&q=" (buffer-substring beg end))))
+(global-set-key (kbd "C-M-g") 'google-region)
+
+(require 'gist)
+
+(load "~/.emacs.d/plugins/nxhtml/autostart")
+
+;; MIT Scheme
+(setenv "MITSCHEME_LIBRARY_PATH"  "/Applications/mit-scheme.app/Contents/Resources")
